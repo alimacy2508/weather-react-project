0fd2f9e9ecca419fff4ed805ffce4c76ceaa2a05
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-10-08 10:50:20 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import React from "react";
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/alimacy2508?tab=repositories"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Alice
-        </a>{" "}
-        and this code is open-sourced on {""}
-        <a
-          href="https://github.com/alimacy2508/weather-react-project"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>
-      </footer>
+      <div className="container">
+        Weather App
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/alimacy2508?tab=repositories"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Alice
+          </a>{" "}
+          and this code is open-sourced on {""}
+          <a
+            href="https://github.com/alimacy2508/weather-react-project"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
