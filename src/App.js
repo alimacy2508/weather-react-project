969f8340d77c2c63d9acadcd4af35e278a64df5a
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-10-08 10:10:22 +0100

    Added open source links to my github proflie and repository

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import "./App.css";
 import React from "react";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/alimacy2508?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Alice
+        </a>{" "}
+        and this code is open-sourced on {""}
+        <a
+          href="https://github.com/alimacy2508/weather-react-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>
+      </footer>
+    </div>
+  );
 }
