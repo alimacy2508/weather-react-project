78c5ab703f7399c6f867cdd3c2c0cddf59854cd2
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-10-08 11:01:45 +0100

    Added first component with basic weather html

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React from "react";
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        Weather App
+        <Weather />
         <footer>
           This project was coded by{" "}
           <a
---
+++ src/Weather.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <h1>Paris</h1>
+      <ul>
+        <li>Sunday, 11:00</li>
+        <li>Mostly Sunny</li>
+      </ul>
+      Hello from my weather forecast
+    </div>
+  );
+}
