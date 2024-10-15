bcc0b48ba953d189af0fce0e0c51503e37067902
alimacy2508 <70335280+alimacy2508@users.noreply.github.com>
2024-10-15 14:53:23 +0100

    Forecast icons correctly display the next few days of weather for the corresponding city

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./WeatherForecast.css";
 import axios from "axios";
@@ -7,6 +7,17 @@ import WeatherForecastDay from "./WeatherForecastDay.js";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.city]);
+
+  function loadForecast() {
+    const apiKey = "bbco0f50t952db4519a3613cfbc4bfb3";
+    let city = props.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
   function handleResponse(response) {
     setForecast(response.data.daily);
@@ -18,22 +29,21 @@ export default function WeatherForecast(props) {
       <div className="WeatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
+            if (index > 0 && index < 6) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
         </div>
       </div>
     );
   } else {
-    const apiKey = "bbco0f50t952db4519a3613cfbc4bfb3";
-    let city = "London";
-    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    loadForecast();
     return null;
   }
 }
